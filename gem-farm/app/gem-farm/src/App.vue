a633d2b1b676f61e909caa4efa2788af2fcc6241
web-dev-02 <you@example.com>
2022-07-25 13:33:33 +0530

    first commit

---
+++ gem-farm/app/gem-farm/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="relative h-full min-h-screen">
+    <TheNavBar />
+    <div class="pt-10 px-10 flex justify-center align-middle">
+      <img :width="300" :height="50" src="./assets/technomads.png" />
+    </div>
+    <div class="italic mt-5 text-center">by Technomads</div>
+
+    <div class="p-10">
+      <router-view />
+    </div>
+
+    <div class="pt-10"></div>
+  </div>
+</template>
+
+<script>
+import TheNavBar from '@/components/TheNavBar';
+import TheFooter from '@/components/gem-farm/TheFooter';
+import TheCat from '@/components/gem-farm/TheCat';
+export default {
+  components: { TheCat, TheFooter, TheNavBar },
+};
+</script>
+
+<style>
+* {
+  font-family: 'Press Start 2P', monospace;
+}
+input[type='radio']:checked + span {
+  @apply text-black;
+}
+</style>
---
+++ gem-farm/app/gem-farm/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex flex-row">
+    <div class="nes-container with-title flex-1 mr-5">
+      <p class="title">Start/manage a farm</p>
+      <p class="mb-5">
+        Choose this if you have your own NFT collection you'd like to offer
+        staking for.
+      </p>
+      <router-link to="manager">
+        <button class="nes-btn is-primary">Start a farm</button>
+      </router-link>
+    </div>
+    <div class="nes-container with-title flex-1 ml-5">
+      <p class="title">Stake your NFTs</p>
+      <p class="mb-5">
+        Choose this if you'd like to stake your NFTs at one of the existing
+        farms.
+      </p>
+      <router-link to="farmer">
+        <button class="nes-btn is-primary">Stake NFTs</button>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { GEM_FARM_PROG_ID } from '@gemworks/gem-farm-ts';
+export default defineComponent({
+  setup() {
+    return {
+      GEM_FARM_PROG_ID,
+    };
+  },
+});
+</script>
+<style scoped></style>
